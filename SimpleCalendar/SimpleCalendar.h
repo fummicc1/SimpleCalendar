2f5dd5d6fdd830aa26cd6f7b229b52dea2afef17
fummicc1 <fumiya.tennis1@gmail.com>
2020-10-30 08:36:15 +0900

    Initial Commit

---
+++ SimpleCalendar/SimpleCalendar.h
@@ -0,0 +1,18 @@
+//
+//  SimpleCalendar.h
+//  SimpleCalendar
+//
+//  Created by Fumiya Tanaka on 2020/10/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleCalendar.
+FOUNDATION_EXPORT double SimpleCalendarVersionNumber;
+
+//! Project version string for SimpleCalendar.
+FOUNDATION_EXPORT const unsigned char SimpleCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleCalendar/PublicHeader.h>
+
+
